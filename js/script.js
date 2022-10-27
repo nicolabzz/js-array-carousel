cff76b725a945336676501891df1f39b7d6fba35
nicolabzz <nbuzzigoli@gmail.com>
2022-10-27 17:47:01 +0200

    .standard ex done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const arrImg = ['assets/img/01.jpg', 'assets/img/02.jpg', 'assets/img/03.jpg', 'assets/img/04.jpg', 'assets/img/05.jpg'];
-
 const btnTop = document.querySelector('.btn-top');
 const btnBottom = document.querySelector('.btn-bottom');
 const container = document.querySelector('.container');
@@ -8,21 +7,59 @@ const container = document.querySelector('.container');
 //this 'for' will cycle 5 times, because there are five img in the array
 for (let i = 0; i < arrImg.length; i++) {
     
-    const img = document.createElement('img'); //I create a variable img that contais the HTML tag element 'img'
+   const imgE = document.createElement('img'); //I create a variable img that contais the HTML tag element 'img'
     
-    img.src = arrImg[i];
+    imgE.src = arrImg[i];
     
-    img.classList.add('slider-img');
+    imgE.classList.add('slider-img');
     
     if (i === 0) //this if will be used only in the first cycle so the container will display at leat something instead of be empty
     {
-        img.classList.add('show'); //the class "slider-img" has a d-none value to hide the back pics, 'show' has d-block value, only the first pic in this 'for' will be affected by this
+        imgE.classList.add('show'); //the class "slider-img" has a d-none value to hide the back pics, 'show' has d-block value, only the first pic in this 'for' will be affected by this
     }
     
-    container.append(img); //with this method the current img will be added to the container
+    container.append(imgE); //with this method the current img will be added to the container
     
 };
 
+//now we are going to select all the images we have created and we will put them into a varable that we will call 'imgList'
+const imgList = document.querySelectorAll('.slider-img');
 
+
+let i = 0; //counter
+
+btnBottom.addEventListener('click', function() {
+
+    imgList[i].classList.remove('show');
+
+    i++;
+
+    imgList[i].classList.add('show');
+
+    btnTop.classList.remove('hidden');
+
+    if (i === imgList.length - 1)
+    {
+        btnBottom.classList.add('hidden');
+    }
+
+});
+
+btnTop.addEventListener('click', function() {
+
+    imgList[i].classList.remove('show');
+
+    i--;
+
+    imgList[i].classList.add('show');
+
+    btnBottom.classList.remove('hidden');
+
+    if (i === 0)
+    {
+        btnTop.classList.add('hidden');
+    }
+
+});
 
 
